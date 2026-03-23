c5e88921a0a9037b61304e9659464982a328649f
shinokun <hypatia.tile02021@gmail.com>
2026-03-23 21:17:03 +0900

    mod: update init.sh to create arbitrary project with custom name

--- init.sh
+++ init.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-mkdir -p parser-demo
-cd parser-demo
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <project-name>"
+  exit 1
+fi
+
+project_name=$1
+
+mkdir -p "$project_name"
+cd "$project_name"
 cabal init --non-interactive \
-  --package-name=parser-demo \
+  --package-name="$project_name" \
   --license=MIT \
   --libandexe \
   --tests \
-  --main-is=LoxCli.hs \
+  --main-is=Main.hs \
   --language=GHC2021
