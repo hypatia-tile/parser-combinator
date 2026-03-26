3688545d8a136fef4f02f43991099112924170f6
shinokun <hypatia.tile02021@gmail.com>
2026-03-26 23:56:15 +0900

    feat: add primitive parser example
    I need them for comprehension

--- megaparsec-tutorial/src/MyLib.hs
+++ megaparsec-tutorial/src/MyLib.hs
@@ -1,4 +1,25 @@
 module MyLib (someFunc) where
 
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Set
+
+type Parser = Parsec Void String
+
+testText :: Parser Char
+testText = satisfy (== 'a')
+
+runTest :: String -> Either (ParseErrorBundle String Void) Char
+runTest input = parse testText "runTest" input
+
+runTest2 :: String -> IO ()
+runTest2 input =
+  case runTest input of
+    Left err -> putStr $ errorBundlePretty err
+    Right c -> putStrLn $ "Parsed character: " ++ [c]
+
+
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
