071a386cc8f743a3fe5cdf3e7c045bda06379c41
shinokun <hypatia.tile02021@gmail.com>
2026-03-30 12:46:28 +0900

    feat: define a parser monad wrapping an innner monad

---
+++ megaparsec-tutorial/src/Example02/ParserMT.hs
@@ -0,0 +1,25 @@
+module Example02.ParserMT where
+
+newtype ParserMT m a = PMT { runPMT :: String -> m (a, String) }
+
+type LexerM = ParserMT Maybe
+
+instance (Functor m) => Functor (ParserMT m) where
+  -- fmap (a -> b) -> ParserMT m a -> ParserMT m b
+  fmap f p = PMT $ \inp -> fmap (\(x, rest) -> (f x, rest)) (runPMT p inp)
+
+instance (Monad m) => Applicative (ParserMT m) where
+  -- pure :: m => a -> ParserMT a
+  pure x = PMT $ \inp -> pure (x, inp)
+  -- (<*>) :: ParserMT m (a -> b) -> ParserMT m a -> ParserMT m b
+  (PMT f) <*> (PMT q) = PMT $ \inp -> do
+    (f', s) <- f inp
+    (q', s') <- q s
+    return (f' q', s')
+
+instance (Monad m) => Monad (ParserMT m) where
+  return = pure
+  (PMT p) >>= f = PMT $ \inp -> do
+    (v, res) <- p inp
+    runPMT (f v) res
+
