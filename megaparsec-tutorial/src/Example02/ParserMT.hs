43e35797182e35c02c026f33af4713cd00a550ae
shinokun <hypatia.tile02021@gmail.com>
2026-03-30 22:55:49 +0900

    feat: add MonadPlus and MonadState instnces to ParserMT

--- megaparsec-tutorial/src/Example02/ParserMT.hs
+++ megaparsec-tutorial/src/Example02/ParserMT.hs
@@ -1,6 +1,9 @@
 module Example02.ParserMT where
 
-newtype ParserM m a = PMT { runPMT :: String -> m (a, String) }
+import Control.Applicative (Alternative (..))
+import Control.Monad (MonadPlus (..))
+
+newtype ParserM m a = PMT {runPMT :: String -> m (a, String)}
 
 type LexerM = ParserM Maybe
 
@@ -11,6 +14,7 @@ instance (Functor m) => Functor (ParserM m) where
 instance (Monad m) => Applicative (ParserM m) where
   -- pure :: m => a -> ParserMT a
   pure x = PMT $ \inp -> pure (x, inp)
+
   -- (<*>) :: ParserMT m (a -> b) -> ParserMT m a -> ParserMT m b
   (PMT f) <*> (PMT q) = PMT $ \inp -> do
     (f', s) <- f inp
@@ -22,4 +26,54 @@ instance (Monad m) => Monad (ParserM m) where
   (PMT p) >>= f = PMT $ \inp -> do
     (v, res) <- p inp
     runPMT (f v) res
+
+instance (Alternative m, Monad m) => Alternative (ParserM m) where
+  empty = PMT $ \_ -> empty
+  (PMT p) <|> (PMT q) = PMT $ \inp ->
+    p inp <|> q inp
+
+instance (MonadPlus m, Monad m) => MonadPlus (ParserM m) where
+  mzero = empty
+  mplus = (<|>)
+
+newtype StateMonad s m a = S { runState :: s -> m (a, s) }
+
+instance (Functor m) => Functor (StateMonad s m) where
+  fmap f (S st) = S $ fmap (\(x, st') -> (f x, st')) . st
+
+instance (Monad m) => Applicative (StateMonad s m) where
+  pure x = S $ \st -> pure (x, st)
+  (S f) <*> (S st) = S $ \x -> do
+    (u, s) <- f x
+    (v, s') <- st s
+    return (u v, s')
+
+instance (Monad m) => Monad (StateMonad s m) where
+  return = pure
+  (S s) >>= f = S $ \st -> do
+    (x, u) <- s st
+    runState (f x) u
+
+class (Monad m) => MonadState s m where
+  fetch :: m s
+  put :: s -> m ()
+  state :: (s -> (a, s)) -> m a
+
+update :: (MonadState s m) => (s -> s) -> m ()
+update f = do
+  x <- fetch
+  _ <- put (f x)
+  return ()
+
+instance (Monad m) => MonadState s (StateMonad s m) where
+  -- fetch :: StateMonad s m s
+  fetch = S $ \s -> return (s, s)
+  -- put :: s -> StateMonad s m ()
+  put s = S $ \_ -> return ((), s)
+  -- state :: (s -> (a, s)) -> StateMonad s m a
+  state f = do
+    s <- fetch
+    let (v, s') = f s
+    put s'
+    return v
 
