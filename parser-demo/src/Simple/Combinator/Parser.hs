58aac73244d8449ee4df8fc215df4dbb7b9e17c8
shinokun <hypatia.tile02021@gmail.com>
2026-06-24 08:37:50 +0900

    feat: instanciate Parser for monad

--- parser-demo/src/Simple/Combinator/Parser.hs
+++ parser-demo/src/Simple/Combinator/Parser.hs
@@ -1,9 +1,25 @@
 module Simple.Combinator.Parser (
+    Parser,
     ParserT (runParser),
     mkParser,
 ) where
+
+import Data.Functor.Identity (Identity)
 
 newtype ParserT m a = P {runParser :: String -> m (a, String)}
 
 mkParser :: (String -> m (a, String)) -> ParserT m a
 mkParser = P
+
+type Parser a = ParserT Identity a
+
+instance (Functor m) => Functor (ParserT m) where
+    fmap f (P p) = P $ \inp -> fmap (\(x, y) -> (f x, y)) (p inp)
+
+instance (Monad m) => Applicative (ParserT m) where
+  pure x = P $ \inp -> pure (x, inp)
+  (P f) <*> (P x) = P $ \inp -> do (f', res) <- f inp; (x', res') <- x res; return (f' x', res')
+
+instance (Monad m) => Monad (ParserT m) where
+  return = pure
+  (P x) >>= f = P $ \inp -> do (x', res) <- x inp; runParser (f x') res
