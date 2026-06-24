3ba98c43a8e3726d858e94bd57dd78d58d572552
shinokun <hypatia.tile02021@gmail.com>
2026-06-24 09:03:35 +0900

    fix: remove redundant implementation

--- parser-demo/src/SimpleLexer.hs
+++ parser-demo/src/SimpleLexer.hs
@@ -1,49 +1,13 @@
 module SimpleLexer where
 
-import Text.ParserCombinators.ReadP
 import Data.Char (isDigit, isAlpha, isAlphaNum)
+import Simple.Combinator.Parser
 
-data Token
-  = TInt Int
+data Token a
+  = TInt a
   | TPlus
   | TMul
   | TLParen
   | TRParen
-  | TIdent String
   deriving (Show, Eq)
-
-identP :: ReadP Token
-identP = TIdent <$> ((:) <$> satisfy isAlpha <*> munch isAlphaNum)
-
--- Parse One token, skipping leading whitespaces.
-tokenP :: ReadP Token
-tokenP =
-  ( (TInt . read <$> munch1 isDigit)
-    <++ identP
-    <++ (TPlus <$ char '+')
-    <++ (TMul <$ char '*')
-    <++ (TLParen <$ char '(')
-    <++ (TRParen <$ char ')')
-  )
-
--- Parse all tokens until end of input.
-tokensP :: ReadP [Token]
-tokensP = skipSpaces *> many (tokenP <* skipSpaces) <* eof
-
-lexArithmetic :: String -> Either String [Token]
-lexArithmetic input =
-  case readP_to_S tokensP input of
-    [] -> Left "lexing failed"
-    xs ->
-      case [ts | (ts, rest) <- xs, rest == ""] of
-        [] -> Left "lexing failed: unconsumed input"
-        ts : _ -> Right ts
-
-tokenStep :: String -> Maybe (Token, String)
-tokenStep input =
-  case readP_to_S tokenP input of
-    [] -> Nothing
-    xs -> case reverse xs of
-      [] -> Nothing
-      ((tok, rest) : _) -> Just (tok, rest)
 
